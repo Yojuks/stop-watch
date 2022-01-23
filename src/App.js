0fe90fe035645542838371675e916ef0b115098f
AndriiYozh <yojuks@gmail.com>
2022-01-23 22:45:44 +0200

    Add rxjs functional

--- src/App.js
+++ src/App.js
@@ -1,59 +1,70 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
+import { useEffect, useState } from "react";
+import { interval, Subject, fromEvent } from "rxjs";
+import { takeUntil, map, buffer, debounceTime, filter } from "rxjs/operators";
 import "./App.css";
 
 function App() {
-  const [time, setTime] = useState(0);
-  const [timerOn, setTimerOn] = useState(false);
-
   const [timer, setTimer] = useState(0);
-
-  let timerValue = useRef(timer);
-
-  const handleClick = (e) => {
-    clearTimeout(timerValue);
-
-    timerValue = setTimeout(() => {
-      // e.detail === 2 need for a check double click
-      if (e.detail === 2) {
-        setTimerOn(false);
-      }
-    }, 300);
-    console.log(timerValue);
-  };
+  const [isActive, setIsActive] = useState(false);
 
   useEffect(() => {
-    let interval = null;
+    let stream$ = new Subject();
+    let timerObservable = interval(1000);
+    timerObservable.pipe(takeUntil(stream$)).subscribe(() => {
+      if (isActive === true) {
+        setTimer((prev) => prev + 1000);
+      }
+    });
 
-    if (timerOn) {
-      interval = setInterval(() => {
-        setTime((prevTime) => prevTime + 10);
-      }, 10);
-    } else {
-      clearInterval(interval);
+    return () => {
+      stream$.next();
+      stream$.complete();
+      stream$.unsubscribe();
+    };
+  }, [isActive]);
+
+  const start = () => {
+    if (isActive !== true) {
+      setIsActive(true);
     }
-    return () => clearInterval(interval);
-  }, [timerOn]);
+  };
 
-  // You should answer me why we should create this function in such way for onClick callback
-  const onStart = useCallback(() => {
-    setTimerOn(true);
-  }, [setTimerOn]);
+  const stop = () => {
+    if (isActive === true) {
+      setIsActive(false);
+    }
+  };
+
+  const wait = () => {
+    const mouse$ = fromEvent(document.querySelector("#wait"), "click");
+    const buff$ = mouse$.pipe(debounceTime(300));
+    const click$ = mouse$.pipe(
+      buffer(buff$),
+      map((list) => {
+        return list.length;
+      }),
+      filter((x) => x === 2)
+    );
+    click$.subscribe(() => {
+      setIsActive(false);
+    });
+  };
+
+  const reset = () => {
+    setTimer(0);
+    setIsActive(true);
+  };
 
   return (
     <div className="App">
-      <div className="timer">
-        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2) + " : "}</span>
-        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2) + " : "}</span>
-        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
-      </div>
-      <div>
-        {!timerOn && <button onClick={onStart}>Start</button>}
-
-        {<button onClick={() => setTimerOn(false)}>Stop</button>}
-
-        {timerOn && <button onClick={() => setTime(0)}>Reset</button>}
-
-        {timerOn && <button onClick={handleClick}>Wait</button>}
+      <div className="timer">{new Date(timer).toISOString().slice(11, 19)}</div>
+      <div className="buttons">
+        {!isActive && <button onClick={start}>start</button>}
+        <button onClick={stop}>stop</button>
+        <button onClick={reset}>reset</button>
+        <button id="wait" onClick={wait}>
+          wait
+        </button>
       </div>
     </div>
   );
